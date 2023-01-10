72c188f3bdfde968c1d14f410bae28332c987111
MERIEM WAFA YOUSFI <m.yousfi@meteo.dz>
2023-01-10 15:06:27 +0100

    created the authentification

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 import { initializeApp } from 'firebase/app';
-import {getAuth, signInWithRedirect, signInWithPopup,GoogleAuthProvider, getAdditionalUserInfo, createUserWithEmailAndPassword} from 'firebase/auth';
+import {getAuth,
+        signInWithRedirect,
+        signInWithPopup,
+        GoogleAuthProvider,
+        getAdditionalUserInfo,
+        createUserWithEmailAndPassword,
+        signInWithEmailAndPassword} from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -49,5 +55,11 @@ const firebaseConfig = {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth, email, password)
+
+  }
+  export const signInAuthUserWithEmailAndPassword = async (email, password) =>{
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password)
 
   }
